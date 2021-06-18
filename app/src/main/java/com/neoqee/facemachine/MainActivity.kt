8155800abd45ee2c1ad5674db0763efa626ec5fb
zhujunwen <804187225@qq.com>
2021-06-18 15:52:04 +0800

    init project

---
+++ app/src/main/java/com/neoqee/facemachine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neoqee.facemachine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
